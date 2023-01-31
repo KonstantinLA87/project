b5431019981a94fefb9e249f0cdf0c67650cb3b7
Kosta <konstantinla87@gmail.com>
2023-02-01 00:12:01 +0300

    Модуль 1.5 добавили все что связано с css и scss компиляцией, генерацией названий классов и тд. МОДУЛЬ 1.5 - очень сложный, лучше пересмотреть как нибудь

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,9 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
+import { BuildOptions } from './types/config';
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+  
   // Typescript
   const typescriptLoader = {
     test: /\.tsx?$/,
@@ -8,5 +11,25 @@ export function buildLoaders(): webpack.RuleSetRule[] {
     exclude: /node_modules/,
   };
 
-  return [typescriptLoader];
+  // SCSS
+  const cssLoader = {
+      test: /\.s[ac]ss$/i,
+      use: [
+        isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+        {
+          loader: "css-loader",
+          options: {
+            modules: {
+              auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+              localIdentName: isDev 
+                ? '[path][name]__[local]--[hash:base64:5]' 
+                : '[hash:base64:8]',
+            },
+          },
+        },
+        "sass-loader",
+      ],
+  };
+
+  return [typescriptLoader, cssLoader];
 }
---
+++ src/components/Counter.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import classes from './Counter.module.scss';
+
+export const Counter = () => {
+  
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  }
+
+  return (
+    <div className={classes.count}>
+      <p>{count}</p>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+};
