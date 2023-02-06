f8ed21aaf454e90fdd2bb24ba93ab81d053a0bfa
Kosta <konstantinla87@gmail.com>
2023-02-06 19:07:58 +0300

    Модуль 2.6 Добавили webpack hot replacement - для обновления стилей без перезагрузки

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -17,6 +17,7 @@ export function buildPlugins({ paths, isDev}: BuildOptions): webpack.WebpackPlug
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin(),
   ];
 }
