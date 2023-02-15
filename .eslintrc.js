9286e8f02016e8f677c588e6f5d9c79fea3d2813
Kosta <konstantinla87@gmail.com>
2023-02-15 12:25:21 +0300

    fix dev plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,32 +2,36 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jest: true
+    jest: true,
   },
-  extends: ['plugin:react/recommended', 'standard-with-typescript', 'plugin:storybook/recommended'],
+  extends: [
+    "plugin:react/recommended",
+    "standard-with-typescript",
+    "plugin:storybook/recommended",
+  ],
   overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: 'tsconfig.json'
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "tsconfig.json",
   },
-  plugins: ['react', 'i18next'],
+  plugins: ["react", "i18next"],
   rules: {
-    '@typescript-eslint/semi': 'off',
-    'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/consistent-type-imports': 'off',
-    '@typescript-eslint/comma-dangle': 'off',
-    '@typescript-eslint/strict-boolean-expressions': 'off',
-    '@typescript-eslint/prefer-nullish-coalescing': 'off',
-    '@typescript-eslint/no-unused-vars': 'warn',
-    '@typescript-eslint/explicit-function-return-type': 'warn',
-    '@typescript-eslint/no-floating-promises': 'warn',
-    '@typescript-eslint/naming-convention': 'off',
-    'i18next/no-literal-string': 2,
-    'no-trailing-spaces': 'off',
-    'i18next/no-literal-string': 'off'
+    "@typescript-eslint/semi": "off",
+    "react/react-in-jsx-scope": "off",
+    // '@typescript-eslint/consistent-type-imports': 'off',
+    "@typescript-eslint/comma-dangle": "off",
+    "@typescript-eslint/strict-boolean-expressions": "off",
+    "@typescript-eslint/prefer-nullish-coalescing": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/explicit-function-return-type": "warn",
+    "@typescript-eslint/no-floating-promises": "warn",
+    "@typescript-eslint/naming-convention": "off",
+    "i18next/no-literal-string": 2,
+    "no-trailing-spaces": "off",
+    "i18next/no-literal-string": "off",
   },
   globals: {
-    '__IS_DEV__': true
-  }
+    __IS_DEV__: true,
+  },
 };
