a4b8bad7030a5643760315ce571724bf6fd784b0
Kosta <konstantinla87@gmail.com>
2023-02-27 21:12:31 +0300

    ПОЛОМКА В 4 модуле в конце, проверить и справить

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,13 +1,17 @@
-import { configureStore, ReducersMapObject } from '@reduxjs/toolkit';
+import { configureStore, DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 import { counterReducer } from 'entities/Counter';
 import { userReducer } from 'entities/User';
 import { createReducerManager } from './reducerManager';
 import { StateSchema } from './StateSchema';
 
-export function createReduxStore(initialState?: StateSchema) {
-    const rootReducers: ReducersMapObject<StateSchema> = {
+export function createReduxStore(
+  initialState?: StateSchema, 
+  asyncReducers?: ReducersMapObject<StateSchema>
+) {
+  const rootReducers: ReducersMapObject<StateSchema> = {
+        ...asyncReducers,
         counter: counterReducer,
-        user: userReducer
+        user: userReducer,
     };
 
     const reducerManager = createReducerManager(rootReducers);
--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -1,17 +1,21 @@
 import { FC, ReactNode } from 'react';
 import { Provider } from 'react-redux';
-import { DeepPartial } from 'redux';
+import { DeepPartial, ReducersMapObject } from 'redux';
 import { StateSchema } from '../config/StateSchema';
 import { createReduxStore } from '../config/store';
 
 interface StoreProviderProps {
   children?: ReactNode;
-  initialState?: DeepPartial<StateSchema>
+  initialState?: DeepPartial<StateSchema>;
+  asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>
 }
 
-export const StoreProvider: FC<StoreProviderProps> = ({children, initialState}) => {
+export const StoreProvider: FC<StoreProviderProps> = ({children, initialState, asyncReducers}) => {
 
-  const store = createReduxStore(initialState as StateSchema);
+  const store = createReduxStore(
+    initialState as StateSchema, 
+    asyncReducers as ReducersMapObject<StateSchema>
+  );
 
   return (
     <Provider store={store}>
