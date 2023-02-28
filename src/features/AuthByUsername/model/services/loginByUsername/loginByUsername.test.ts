4ea6f66fbc0054bb898242091711eaa2d98125c7
Kosta <konstantinla87@gmail.com>
2023-02-28 14:27:51 +0300

    не проходит тест, не хочет импортировать axios пробую разобраться

---
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
@@ -0,0 +1,28 @@
+import { Dispatch } from "@reduxjs/toolkit";
+import { StateSchema } from "app/providers/StoreProvider";
+import axios from 'axios';
+import { loginByUsername } from "./loginByUsername";
+
+jest.mock('axios');
+
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+// const mockedAxios = jest.mocked(axios, true);
+
+describe('loginByUsername', () => {
+  let dispatch: Dispatch;
+  let getState: () => StateSchema;
+
+  beforeEach(() => {
+    dispatch = jest.fn();
+    getState = jest.fn();
+  });
+
+  test('', async () => {
+    // mockedAxios.post.mockReturnValue(Promise.resolve({data: {username: '123', id: '1'}}));
+    mockedAxios.post.mockResolvedValue({data: {username: '123', id: '1'}});
+    const action = loginByUsername({ username: '123', password: '123' });
+    const result = action(dispatch, getState, undefined);
+    console.log(result);
+    // expect().toEqual();
+  });
+});
